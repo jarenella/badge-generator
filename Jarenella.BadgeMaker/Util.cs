ce081edfe70ebfcfa5f489f28da43a14206b87f5
jarenella <jamesa122333@gmail.com>
2023-03-19 19:09:18 -0700

    finished util makecsv method logic to create new file with input data

--- Jarenella.BadgeMaker/Util.cs
+++ Jarenella.BadgeMaker/Util.cs
@@ -22,6 +22,16 @@ namespace Jarenella.BadgeMaker{
             {
                 Directory.CreateDirectory("data");
             }
+            //create new SteamWriter file but only have it be active while we need it / are using it
+            using (StreamWriter file = new StreamWriter("data/employees.csv"))
+            {
+                file.WriteLine("ID,Name,PhotoUrl");
+                for (int i = 0; i < employees.Count; i++)
+                {
+                    string template = "{0},{1},{2}";
+                    file.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
+                }
+            }
         }
     }
 }
